0e33704bf1a4acdd88da0cbf882827a66e1e49c5
Gerrit Meier <meistermeier@gmail.com>
2015-05-10 18:15:55 +0200

    introducing thymeleaf

--- src/main/java/com/meistermeier/mvc/beer/model/Beer.java
+++ src/main/java/com/meistermeier/mvc/beer/model/Beer.java
@@ -2,13 +2,19 @@ package com.meistermeier.mvc.beer.model;
 
 public class Beer {
 
+    private final Long id;
     private final String name;
 
-    public Beer(String name) {
+    public Beer(Long id, String name) {
+        this.id = id;
         this.name = name;
     }
 
     public String getName() {
         return name;
+    }
+
+    public Long getId() {
+        return id;
     }
 }
--- src/main/java/com/meistermeier/mvc/beer/web/BeerController.java
+++ src/main/java/com/meistermeier/mvc/beer/web/BeerController.java
@@ -23,16 +23,16 @@ public class BeerController {
     @GET
     @Controller
     public String beerList() {
-        models.put("beers", newArrayList("Beer1", "Beer2"));
-        return "list.jsp";
+        models.put("beers", newArrayList(new Beer(1L, "tasty"), new Beer(2L, "ugly")));
+        return "list.html";
     }
 
     @Path("{beerId}/")
     @GET
     @Controller
     public Response beerDetails(@PathParam("beerId") String beerId) {
-        models.put("name", "super beer");
-        return Response.status(Response.Status.OK).entity("details.jsp").build();
+        models.put("beer", new Beer(1L, "tasty"));
+        return Response.status(Response.Status.OK).entity("details.html").build();
     }
 
 
@@ -40,7 +40,7 @@ public class BeerController {
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     public Beer jsonDetails(@PathParam("beerId") String beerId) {
-        return new Beer("super beer");
+        return new Beer(1L, "super beer");
     }
 
 }
